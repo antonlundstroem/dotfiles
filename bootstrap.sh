feb73de8a2fd615c2c8920399bd1e141c077befb
antlun <anton.lundstroem@hotmail.se>
2024-11-04 20:05:28 +0100

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ln -s ~/.dotfiles/.config ~/.config
-ln -s ~/.dotfiles//.taskrc ~/.taskrc
+ln -s ~/.dotfiles/.taskrc ~/.taskrc
 ln -s ~/.dotfiles/.wezterm.lua ~/.wezterm.lua
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
